a08aeb4d2d0dd550c36e4ec5ec97747464759e17
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-13 12:31:31 -0600

    Finished the react 2 mini project

--- src/App.js
+++ src/App.js
@@ -9,25 +9,67 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
+  constructor() {
+    super();
+
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true'
+    }
+    this.updateColor = this.updateColor.bind(this);
+    this.updateSize = this.updateSize.bind(this);
+    this.updateFamily = this.updateFamily.bind(this);
+    this.updatedEditStatus = this.updatedEditStatus.bind(this);
+  }
+
+  // You could create a unversal handler to do what the other ones are doing below with an event.target.value... this would be for later down the road.
+
+  // universalHandler(e) {
+
+  // }
 
   // updateColor
-
+  updateColor(val) {
+    this.setState({fontColor: val})
+  }
   // updateSize
-
+  updateSize(val) {
+    this.setState({fontSize: val})
+  }
   // updateFamily
-
+  updateFamily(val) {
+    this.setState({fontFamily: val})
+  }
   // updateEditStatus
+  updatedEditStatus(val) {
+    this.setState({allowEdit: val})
+  }
 
   render() {
+    // const {allowEdit, fontFamily, fontColor, fontSize} = this.state
     return (
       <div>
         <div className="headerBar">
-          {/* Render EditToggle */}
-          {/* Render ColorChanger */}
-          {/* Render SizeChanger */}
-          {/* Render FamilyChanger */}
+        <EditToggle 
+          update    = {this.updatedEditStatus} />
+        <ColorChanger 
+          update    = {this.updateColor} 
+          allowEdit = {this.state.allowEdit}/>
+        <SizeChanger 
+          update    = {this.updateSize} 
+          allowEdit = {this.state.allowEdit}/>
+        <FamilyChanger 
+          update    = {this.updateFamily} 
+          allowEdit = {this.state.allowEdit}/>
         </div>
-        <div className="textArea">{/* Render TextContainer */}</div>
+        <div className="textArea">
+        <TextContainer 
+          fontSize   = {this.state.fontSize} 
+          fontColor  = {this.state.fontColor} 
+          fontFamily = {this.state.fontFamily} />
+        </div>
       </div>
     );
   }
